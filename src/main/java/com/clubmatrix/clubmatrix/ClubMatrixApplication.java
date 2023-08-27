6eedb9817d85cfa960b23666dbbf02d1734d0dab
André Bortoli <andre.ugsto@proton.me>
2023-08-27 07:00:15 -0300

    chore: First commit

---
+++ src/main/java/com/clubmatrix/clubmatrix/ClubMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.clubmatrix.clubmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clubmatrix/clubmatrix/ClubMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubmatrix.clubmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
