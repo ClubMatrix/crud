eac775ad2446c7656f104ce4716f8ade4f1579b7
André Bortoli <andre.ugsto@proton.me>
2023-08-28 19:33:54 -0300

    fix: Change logic to accept token string instead of login object

--- src/main/java/com/clubmatrix/crud/utils/AuthUtil.java
+++ src/main/java/com/clubmatrix/crud/utils/AuthUtil.java
@@ -1,12 +1,21 @@
 package com.clubmatrix.crud.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.clubmatrix.crud.models.Permission;
 import com.clubmatrix.crud.models.Role;
+import com.clubmatrix.crud.repositories.LoginRepository;
 import com.clubmatrix.crud.models.Login;
 
+@Component
 public class AuthUtil {
+
+    @Autowired
+    private LoginRepository loginRepository;
+
     private final String SECRET_KEY = System.getenv("CLUB_MATRIX_SECRET_KEY");
     private final Algorithm ALGORITHM = Algorithm.HMAC512(SECRET_KEY);
 
@@ -28,7 +37,10 @@ public class AuthUtil {
         }
     }
 
-    public boolean hasPermission(Login login, String requiredPermission) {
+    public boolean hasPermission(String token, String requiredPermission) {
+        String loginEmail = JWT.require(ALGORITHM).build().verify(token).getSubject();
+        Login login = loginRepository.findByEmail(loginEmail);
+
         for (Role role : login.getRoles()) {
             for (Permission permission : role.getPermissions()) {
                 if (permission.getName().equals(requiredPermission)) {
