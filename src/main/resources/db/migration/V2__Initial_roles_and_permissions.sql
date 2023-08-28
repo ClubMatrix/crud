733a735b0ef2b9c81d63fd2e81c3a03f4c49ebba
André Bortoli <andre.ugsto@proton.me>
2023-08-28 07:07:50 -0300

    fix: Update naming plural to singular

--- src/main/resources/db/migration/V2__Initial_roles_and_permissions.sql
+++ src/main/resources/db/migration/V2__Initial_roles_and_permissions.sql
@@ -1,5 +1,5 @@
 -- Inserting permissions
-INSERT INTO permissions (slug, name, description) VALUES
+INSERT INTO permission (slug, name, description) VALUES
 ('view_members', 'View Members', 'Permission to view club members and their details'),
 ('edit_members', 'Edit Members', 'Permission to edit club member details'),
 ('register_activity', 'Register Activity', 'Permission to register a new activity'),
@@ -9,25 +9,25 @@ INSERT INTO permissions (slug, name, description) VALUES
 ('view_employee_summary', 'View Employee Summary', 'Permission to view employee summaries');
 
 -- Inserting roles
-INSERT INTO roles (name, slug, description) VALUES
+INSERT INTO role (name, slug, description) VALUES
 ('employee', 'employee', 'Basic club employee'),
 ('manager', 'manager', 'Club manager with higher level permissions');
 
 -- Setting up role-permission relationships
 
 -- Permissions for employee role
-INSERT INTO role_permissions (role_id, permission_id) VALUES
-((SELECT id FROM roles WHERE slug = 'employee'), (SELECT id FROM permissions WHERE slug = 'view_members')),
-((SELECT id FROM roles WHERE slug = 'employee'), (SELECT id FROM permissions WHERE slug = 'edit_members')),
-((SELECT id FROM roles WHERE slug = 'employee'), (SELECT id FROM permissions WHERE slug = 'register_activity')),
-((SELECT id FROM roles WHERE slug = 'employee'), (SELECT id FROM permissions WHERE slug = 'view_activities'));
+INSERT INTO role_permission (role_id, permission_id) VALUES
+((SELECT id FROM role WHERE slug = 'employee'), (SELECT id FROM permission WHERE slug = 'view_members')),
+((SELECT id FROM role WHERE slug = 'employee'), (SELECT id FROM permission WHERE slug = 'edit_members')),
+((SELECT id FROM role WHERE slug = 'employee'), (SELECT id FROM permission WHERE slug = 'register_activity')),
+((SELECT id FROM role WHERE slug = 'employee'), (SELECT id FROM permission WHERE slug = 'view_activities'));
 
 -- Permissions for manager role
-INSERT INTO role_permissions (role_id, permission_id) VALUES
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'view_members')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'edit_members')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'register_activity')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'view_activities')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'manage_employees')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'set_permissions')),
-((SELECT id FROM roles WHERE slug = 'manager'), (SELECT id FROM permissions WHERE slug = 'view_employee_summary'));
+INSERT INTO role_permission (role_id, permission_id) VALUES
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'view_members')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'edit_members')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'register_activity')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'view_activities')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'manage_employees')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'set_permissions')),
+((SELECT id FROM role WHERE slug = 'manager'), (SELECT id FROM permission WHERE slug = 'view_employee_summary'));
