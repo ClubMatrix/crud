7e2f5edbc0649137de96630d62fa3ecc1680db95
André Bortoli <andre.ugsto@proton.me>
2023-08-29 07:02:43 -0300

    chore(auth): remove unused import in AuthUtil"

--- src/main/java/com/clubmatrix/crud/utils/AuthUtil.java
+++ src/main/java/com/clubmatrix/crud/utils/AuthUtil.java
@@ -2,7 +2,6 @@ package com.clubmatrix.crud.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.clubmatrix.crud.models.Permission;
