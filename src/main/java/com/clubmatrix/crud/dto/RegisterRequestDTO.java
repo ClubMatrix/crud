26a522bc1b4a136660686c8f5cfac42fbb10679a
André Bortoli <andre.ugsto@proton.me>
2023-08-29 07:07:50 -0300

    feat(register): add address to DTO and default constructor to Login

--- src/main/java/com/clubmatrix/crud/dto/RegisterRequestDTO.java
+++ src/main/java/com/clubmatrix/crud/dto/RegisterRequestDTO.java
@@ -1,15 +1,19 @@
 package com.clubmatrix.crud.dto;
 
+import com.clubmatrix.crud.models.Address;
+
 public class RegisterRequestDTO {
   public final String name;
   public final String email;
   public final String password;
   public final String phone;
+  public final Address address;
 
-  public RegisterRequestDTO(String name, String email, String password, String phone) {
+  public RegisterRequestDTO(String name, String email, String password, String phone, Address address) {
     this.name = name;
     this.email = email;
     this.password = password;
     this.phone = phone;
+    this.address = address;
   }
 }
--- src/main/java/com/clubmatrix/crud/services/LoginService.java
+++ src/main/java/com/clubmatrix/crud/services/LoginService.java
@@ -1,8 +1,16 @@
 package com.clubmatrix.crud.services;
 
+import com.clubmatrix.crud.dto.LoginRequestDTO;
+import com.clubmatrix.crud.dto.LoginResponseDTO;
+import com.clubmatrix.crud.dto.RegisterRequestDTO;
+import com.clubmatrix.crud.dto.RegisterResponseDTO;
 import com.clubmatrix.crud.models.Login;
 import com.clubmatrix.crud.repositories.LoginRepository;
+import com.clubmatrix.crud.utils.AuthUtil;
+import com.clubmatrix.crud.utils.HashPasswordUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +18,34 @@ public class LoginService {
 
   @Autowired
   private LoginRepository loginRepository;
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private AuthUtil authUtil;
+
+  public LoginResponseDTO authenticate(LoginRequestDTO loginRequest) {
+    Login storedLogin = loginRepository.findByEmail(loginRequest.email);
+    if (storedLogin != null && passwordEncoder.matches(loginRequest.password, storedLogin.getPassword())) {
+      String token = authUtil.generateToken(storedLogin);
+      return new LoginResponseDTO(token);
+    }
+    return null;
+  }
+
+  public RegisterResponseDTO register(RegisterRequestDTO registerRequest) {
+    if (findByEmail(registerRequest.email) != null) {
+      return null;
+    }
+    Login newLogin = new Login(registerRequest.name, registerRequest.email,
+        HashPasswordUtil.hashPassword(registerRequest.password), registerRequest.phone, null);
+    Login registeredLogin = saveLogin(newLogin);
+    if (registeredLogin != null) {
+      return new RegisterResponseDTO(registeredLogin.getId(), registeredLogin.getName(), registeredLogin.getEmail());
+    }
+    return null;
+  }
 
   public Login findByEmail(String email) {
     return loginRepository.findByEmail(email);
