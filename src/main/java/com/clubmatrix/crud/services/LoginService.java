65957bea8a0b901d6bf4ec672c2c476b0e841bb1
André Bortoli <andre.ugsto@proton.me>
2023-08-29 19:38:51 -0300

    refactor(services): Enhance LoginService with exception handling

--- src/main/java/com/clubmatrix/crud/services/LoginService.java
+++ src/main/java/com/clubmatrix/crud/services/LoginService.java
@@ -4,10 +4,11 @@ import com.clubmatrix.crud.dto.LoginRequestDTO;
 import com.clubmatrix.crud.dto.LoginResponseDTO;
 import com.clubmatrix.crud.dto.RegisterRequestDTO;
 import com.clubmatrix.crud.dto.RegisterResponseDTO;
+import com.clubmatrix.crud.exceptions.DuplicateRecordException;
+import com.clubmatrix.crud.exceptions.AuthenticationFailedException;
 import com.clubmatrix.crud.models.Login;
 import com.clubmatrix.crud.repositories.LoginRepository;
 import com.clubmatrix.crud.utils.AuthUtil;
-import com.clubmatrix.crud.utils.HashPasswordUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,32 +27,29 @@ public class LoginService {
   private AuthUtil authUtil;
 
   public LoginResponseDTO authenticate(LoginRequestDTO loginRequest) {
-    Login storedLogin = loginRepository.findByEmail(loginRequest.email);
+    Login storedLogin = findByEmail(loginRequest.email);
     if (storedLogin != null && passwordEncoder.matches(loginRequest.password, storedLogin.getPassword())) {
       String token = authUtil.generateToken(storedLogin);
       return new LoginResponseDTO(token);
     }
-    return null;
+    throw new AuthenticationFailedException("Invalid email or password.");
   }
 
   public RegisterResponseDTO register(RegisterRequestDTO registerRequest) {
     if (findByEmail(registerRequest.email) != null) {
-      return null;
+      throw new DuplicateRecordException("Email already registered.");
     }
     Login newLogin = new Login(registerRequest.name, registerRequest.email,
-        HashPasswordUtil.hashPassword(registerRequest.password), registerRequest.phone, null);
+        passwordEncoder.encode(registerRequest.password), registerRequest.phone, null);
     Login registeredLogin = saveLogin(newLogin);
-    if (registeredLogin != null) {
-      return new RegisterResponseDTO(registeredLogin.getId(), registeredLogin.getName(), registeredLogin.getEmail());
-    }
-    return null;
+    return new RegisterResponseDTO(registeredLogin.getId(), registeredLogin.getName(), registeredLogin.getEmail());
   }
 
-  public Login findByEmail(String email) {
+  private Login findByEmail(String email) {
     return loginRepository.findByEmail(email);
   }
 
-  public Login saveLogin(Login login) {
+  private Login saveLogin(Login login) {
     return loginRepository.save(login);
   }
 }
